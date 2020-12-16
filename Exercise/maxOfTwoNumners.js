252a9feed6451b8a586ad2b51a72491eae2268ed
8abc <anadelaine.catalla@gmail.com>
2020-12-15 22:59:14 -0800

    javascript exercises

---
+++ Exercise/maxOfTwoNumners.js
@@ -0,0 +1,12 @@
+// Write a function that takes in two numbers and returns the maximum of the two
+
+let number = max(1, 2);
+console.log(number);
+
+function max(num1, num2) {
+  if (num1 > num2) return num1;
+  return num2;
+}
+
+//2)
+return num1 > num2 ? num1 : num2;
